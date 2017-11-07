9e6046d2bfef202337b920547b58f05c2ce5cfec
Bryce Chung <BryceTChung@gmail.com>
2017-11-06 16:01:39 -0800

    Added shop and made the interface cleaner

--- GladiatorArena2.0.py
+++ GladiatorArena2.0.py
@@ -27,7 +27,7 @@ def enemyAttack(playerHp,dodge,enemyDamages,enemyChances):
             print("The enemy swings quickly and deals",enemyDamages[0],"damage")
             print("-----------------------------------")
         else:
-            print("Enemy miss")
+            print("\nEnemy miss\n")
     if attackChoice ==2:
         attackChance = randint(0,100)
         if attackChance<=enemyChances[1]-dodge:
@@ -36,7 +36,7 @@ def enemyAttack(playerHp,dodge,enemyDamages,enemyChances):
             print("The enemy strikes and deals",enemyDamages[1],"damage")
             print("-----------------------------------")
         else:
-            print("Enemy miss")
+            print("\nEnemy miss\n")
     if attackChoice ==3:
         attackChance = randint(0,100)
         if attackChance<=enemyChances[2]-dodge:
@@ -45,7 +45,7 @@ def enemyAttack(playerHp,dodge,enemyDamages,enemyChances):
             print("The enemy lands a heavy blow and deals",enemyDamages[2],"damage")
             print("-----------------------------------")
         else:
-            print("Enemy miss")
+            print("\nEnemy misses\n")
     return(playerHp)
 ###################################
 def playerAttack(Class,enemyHp,attackBonus):
@@ -55,7 +55,7 @@ def playerAttack(Class,enemyHp,attackBonus):
         damages = [3,8,11]
     if Class == "archer":
         damages= [3,6,9]
-    playerChoice = int(input("Pick a move\n1)Light\n2)Medium\n3)Heavy\n"))
+    playerChoice = int(input("\nPick a move\n1)Light\n2)Medium\n3)Heavy\n"))
     if playerChoice == 1:
         attackDmg = damages[0]+attackBonus
         attackChance = randint(0,100)
@@ -65,7 +65,7 @@ def playerAttack(Class,enemyHp,attackBonus):
             print("You deal",attackDmg,"damage")
             print("---------------------------")
         else:
-            print("You miss")
+            print("\nYou miss\n")
     if playerChoice == 2:
         attackDmg = damages[1]+attackBonus
         attackChance = randint(0,100)
@@ -75,7 +75,7 @@ def playerAttack(Class,enemyHp,attackBonus):
             print("You deal",attackDmg,"damage")
             print("---------------------------")
         else:
-            print("you miss")
+            print("\nYou miss\n")
     if playerChoice == 3:
         attackDmg = damages[2]+attackBonus
         attackChance = randint(0,100)
@@ -85,37 +85,43 @@ def playerAttack(Class,enemyHp,attackBonus):
             print("You deal",attackDmg,"damage")
             print("---------------------------")
         else:
-            print("you miss")
+            print("\nYou miss\n")
     else:
         ("You trip and nearly fall!")
     return enemyHp
 ########################################################        
 def enemy(Class,hpBonus,attackBonus,dodgeBonus,enemyHp,enemyDamages,enemyType,enemyChances):
     if enemyType ==1:
-        print("Enemy is light")
+        print("\nEnemy is light\n")
     if enemyType ==2:
-        print("Enemy is medium")
+        print("\nEnemy is medium\n")
     if enemyType ==3:
-        print("Enemy is heavy")
+        print("\nEnemy is heavy\n")
     playerHp=20 + hpBonus
     dodge = 10 + dodgeBonus
     while enemyHp >0:
+        print("---------------------------")
         print("You have",playerHp,"hp")
         print("The enemy has",enemyHp,"hp")
+        print("---------------------------")
         enemyHp=playerAttack(Class,enemyHp,attackBonus)
         if enemyHp <=0:
-            print("Enemy killed")
+            print("Enemy killed!")
             break
         playerHp= enemyAttack(playerHp,dodge,enemyDamages,enemyChances)
         if playerHp<=0:
             deathScreen()
     return
+####################################################
+
+    
 #####################################################
 def fight():
     hpBonus=0
     attackBonus=0
     dodgeBonus = 0
     i=0
+    gold = 0
     print("knights have extra health, mages do extra damage, archers are highly evasive")
     Class=input("Pick a class: knight, mage, or archer\n")
     if Class== "knight":
@@ -128,21 +134,54 @@ def fight():
         attackBonus = 1
     while i <9:
         enemyType= randint(1,3)
+        goToShop = input("Would you like to visit the shop? (yes/no)\n")
+        if goToShop == "yes":
+            while True:
+                print("\nYou have",gold,"gold pieces")
+                purchase=int(input("pick an item:\n1=+5hp\n2=+2attackBonus\n3=+15Dodge\n4=Leave shop\n"))
+                if purchase ==1:
+                    if gold >=1:
+                        gold = gold-1
+                        hpBonus = hpBonus +5
+                        print("Purchased!")
+                    else:
+                        print("Not enough gold")
+                if purchase ==2:
+                    if gold >=2:
+                        gold = gold-2
+                        attackBonus = attackBonus +2
+                        print("Purchased!")
+                    else:
+                        print("Not enough gold")
+                if purchase ==3:
+                    if gold >=3:
+                        gold = gold-3
+                        dodgeBonus = dodgeBonus +15
+                        print("Purchased!")
+                    else:
+                        print("Not enough gold")
+                if purchase ==4:
+                    break
+            
+            
         if enemyType == 1:
             enemyHp= 10
             enemyDamages=[3,5,7]
             enemyChances=[100,80,70]
             enemy(Class,hpBonus,attackBonus,dodgeBonus,enemyHp,enemyDamages,enemyType,enemyChances)
+            gold = gold +1
         elif enemyType == 2:
             enemyHp= 20
             enemyDamages=[4,7,10]
             enemyChances=[100,60,40]
             enemy(Class,hpBonus,attackBonus,dodgeBonus,enemyHp,enemyDamages,enemyType,enemyChances)
+            gold = gold +2
         elif enemyType == 3:
             enemyHp= 30
             enemyDamages=[5,7,13]
             enemyChances=[80,55,40]
             enemy(Class,hpBonus,attackBonus,dodgeBonus,enemyHp,enemyDamages,enemyType,enemyChances)
+            gold = gold + 3
         i=i+1
 ######################################################################    
 def mainMenu():
